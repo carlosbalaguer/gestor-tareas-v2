193ed3caef41f1a77ccc401c4b31e36de4f1209b
carlosbalaguer <carlosbalaguer5@gmail.com>
2025-11-11 15:57:35 +0100

    added stricter cors policy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,10 +6,23 @@ import routes from "./routes/index.js";
 
 dotenv.config();
 
+const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+
 export const createApp = () => {
 	const app = express();
 
-	app.use(cors());
+	app.use(
+		cors({
+			origin: (requestOrigin, callback) => {
+				if (!requestOrigin)
+					return callback(new Error("Not allowed by CORS"), false);
+				if (allowedOrigins.includes(requestOrigin)) {
+					return callback(null, true);
+				}
+				return callback(null, false);
+			},
+		})
+	);
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 	app.use("/api", routes);
